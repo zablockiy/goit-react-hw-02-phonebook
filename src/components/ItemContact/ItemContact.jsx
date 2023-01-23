35e5702f0150a6aa8e524c9a24bd038ae97390a9
zablockiy <105744371+zablockiy@users.noreply.github.com>
2023-01-23 16:05:34 +0200

    add icons

--- src/components/ItemContact/ItemContact.jsx
+++ src/components/ItemContact/ItemContact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as IconBin } from '../../icons/bin.svg';
 import PropTypes from 'prop-types';
 import style from './itemContact.module.css';
 
@@ -6,7 +7,9 @@ export const ItemContact = ({ id, name, phone, del }) => {
   return (<>
     <li className={style.item}>
       <p className={style.title}>{name} : {phone}</p>
-      <button type="button" className={style.btn} onClick={() => del(id)}> Delete</button>
+      <button type="button" className={style.btn} onClick={() => del(id)}>
+      <IconBin width="20" fill = "white"/>
+      </button>
     </li>
   </>);
 };
